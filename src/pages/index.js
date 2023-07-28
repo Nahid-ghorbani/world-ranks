88dfb6c79b5d8977225acb197f265e2abca59ba2
Nahid <ghorbani.nca@gmail.com>
2023-07-28 12:18:38 +0330

    Build Layout component

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout/Layout";
+
+function Home() {
+  return <Layout>nahid</Layout>
+}
+
+export default Home;
