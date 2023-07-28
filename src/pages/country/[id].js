621b07e193ab135f91e878686b1fa69f75fef171
Nahid <ghorbani.nca@gmail.com>
2023-07-28 18:31:54 +0330

    get server side props (ssr)

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,5 +1,17 @@
-const Country = () => {
-  return <div>Country</div>;
+import Layout from "@/src/components/Layout/Layout";
+
+const Country = ({ country }) => {
+  console.log(country);
+  return <Layout title={country.name}>Country</Layout>;
 };
 
 export default Country;
+
+export const getServerSideProps = async ({ params }) => {
+  const res = await fetch(`https://restcountries.com/v3.1/alpha/${params.id}`);
+
+  const country = await res.json();
+  return {
+    props: { country },
+  };
+};
